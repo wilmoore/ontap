3660f956d60f4c5da393cbf450b7a6686550a211
Wil Moore III <wil.moore@wilmoore.com>
2013-10-11 10:50:29 -0600

    better reporting on warnings and usagestring

--- ontap.sh
+++ ontap.sh
@@ -3,14 +3,14 @@ function ontap {
   local APPNAME='ontap'
   local VERSION='0.0.1'
   local ONTAPRC=~/.ontaprc
+  local CURYEAR='2013'
 
   # argument parsing
   case "$1" in
 
-    install)
+    "")
 
-      while read line
-      do 
+      while read line; do 
 
         # skip commented or blank lines
         [[ "$line" =~ ^\s?# ]] && continue
@@ -21,9 +21,19 @@ function ontap {
         local formula=$(echo $line | cut -d" " -f2)
         local taprepo=$formula
 
-        # install or tap
-        [[ "$command" = "install" ]] && brew install "$formula"
-        [[ "$command" = "tap"     ]] && brew tap     "$taprepo"
+        # install
+        if [[ "$command" = "install" ]]; then
+          brew install "$formula"
+          continue
+        fi
+
+        # tap
+        if [[ "$command" = "tap"     ]]; then
+          brew tap --repair "$taprepo" 2>/dev/null | head -1 | grep -sqE '^Pruned 0' && \
+            printf "\e[4;31m%s\e[0m: %s\n" "Warning" "$taprepo already tapped"       && \
+            continue
+          brew tap "$taprepo"
+        fi
 
       done <$ONTAPRC
 
@@ -33,16 +43,26 @@ function ontap {
     -v|--version)
 
       echo "$APPNAME version $VERSION"
+      echo ""
 
       return 0
       ;;
 
     -h|--help|*)
 
-      echo "$APPNAME $VERSION"
-      echo ''
-      echo "Usage: $APPNAME install"
-      echo ''
+      cat <<USAGESTRING
+      Usage: ${APPNAME}
+
+        Options:
+
+         -v, --version  Display the version of ${PROGNAME}.
+         -h, --help     Display help information.
+
+      ----
+      ${APPNAME}@${VERSION}
+      © 2013-${CURYEAR} Wil Moore III, All rights reserved.
+
+USAGESTRING
 
       return 0
       ;;
@@ -50,5 +70,3 @@ function ontap {
   esac
 
 }
-
-ontap install
